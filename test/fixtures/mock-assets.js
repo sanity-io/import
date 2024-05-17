a7abfa1eb1610f9fbfbcf52d2a20c32111bd3d14
Michael Wain <michael@sanity.io>
2024-05-17 13:59:56 +0100

    fix(assets): group asset patches per document
    Groups patching of assets per document to reduce lock contention when concurrently
    executing mutations.
    
    ```
    {
      "mutations": [
        {
          "patch": {
            "id": "a-doc-id",
            "set": {
              "pathA.asset": {
                "_ref": "image-foo-200x200-png",
                "_type": "reference"
              },
              "pathB.asset": {
                "_ref": "image-bar-200x200-png",
                "_type": "reference"
              }
            },
            "setIfMissing": {
              "pathA": {
                "_type": "image"
              },
              "pathB": {
                "_type": "image"
              }
            }
          }
        }
      ]
    }
    ```

--- test/fixtures/mock-assets.js
+++ test/fixtures/mock-assets.js
@@ -1,12 +1,14 @@
-module.exports = (url) => {
+module.exports = (urls) => {
   const docs = []
   for (let i = 1; i <= 60; i++) {
-    docs.push({
-      documentId: `doc_${i}`,
-      path: 'some.path',
-      url: url,
-      type: 'image',
-    })
+    for (let j = 0; j < urls.length; j++) {
+      docs.push({
+        documentId: `doc_${i}`,
+        path: `some.path${j}`,
+        url: urls[j],
+        type: 'image',
+      })
+    }
   }
 
   return docs
